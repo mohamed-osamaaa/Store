13b8c2d124714377fc93b74a374d323dac182593
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-02-09 18:45:15 +0200

    commit9

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,7 @@ import {
 
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<ProductPage />} />
       </Routes>
 
       <Toaster />
